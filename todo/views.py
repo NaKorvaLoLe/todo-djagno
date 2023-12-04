ea55cac6d302d0e805b116d3981d3140638a56fe
Nakorvalole <cwotka@yandex.ru>
2023-12-04 06:28:50 +0300

    Создание задач через форму и запись в БД

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
+
+from .forms import TodoForm
 # Create your views here.
 
 
@@ -50,7 +52,18 @@ def logoutuser(request):
         logout(request)
         return redirect('home')
     
-
+def createtodo(request):
+    if request.method == 'GET':
+        return render(request, "todo/createtodo.html", {'form': TodoForm()})
+    else:
+        try:
+            form = TodoForm(request.POST)
+            newtodo = form.save(commit=False)
+            newtodo.user = request.user
+            newtodo.save()
+            return redirect ('currenttodos')
+        except ValueError:
+            return render(request, "todo/createtodo.html", {'form': TodoForm(), 'error':'Переданы не верные данные, попробуйте еще раз'})
 def currenttodos(request):
     return render(request, 'todo/currenttodos.html')    
     
