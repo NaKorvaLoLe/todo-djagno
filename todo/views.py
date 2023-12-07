49b75bb5d850df29ef24ccf14a60ae2f029fefe1
Nakorvalole <cwotka@yandex.ru>
2023-12-07 04:23:01 +0300

    Завершение и удаление задачи

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login, logout, authenticate
+from django.utils import timezone
 
 from .forms import TodoForm
 from .models import Todo
@@ -86,7 +87,17 @@ def viewtodo(request, todo_pk):
         except ValueError:
              return render(request, 'todo/viewtodo.html', {'todo':todo, 'form':form, 'error':'Произошла ошибка :('})    
             
-                
+def completetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk = todo_pk, user = request.user)          
+    if request.method == 'POST':
+        todo.datecompleted = timezone.now()
+        todo.save()
+        return redirect('currenttodos')
     
+def deletetodo(request, todo_pk):
+    todo = get_object_or_404(Todo, pk = todo_pk, user = request.user)          
+    if request.method == 'POST':
+        todo.delete()
+        return redirect('currenttodos')
     
 
